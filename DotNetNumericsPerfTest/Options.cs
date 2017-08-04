0f68d15d281a230467db530e0e46c23592b6e2db
Laszlo Lukacs <laci.28@outlook.com>
2017-08-04 09:37:02 +0200

    Initial commit of the solution

---
+++ DotNetNumericsPerfTest/Options.cs
@@ -0,0 +1,40 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="Options.cs" company="Laszlo Lukacs">
+//   See LICENSE for details.
+// </copyright>
+// --------------------------------------------------------------------------------------------------------------------
+
+namespace DotNetNumericsPerfTest
+{
+    using System.Text;
+    using CommandLine;
+
+    /// <summary>
+    /// Contains the available command line options.
+    /// </summary>
+    public class Options
+    {
+        /// <summary>
+        /// Gets or sets the number of test iterations.
+        /// </summary>
+        [Option('i', "iterations", DefaultValue = 10000000, HelpText = "Number of calculation iterations to run. Default value is 10000000.")]
+        public int Iterations { get; set; }
+
+        /// <summary>
+        /// Gets the usage.
+        /// </summary>
+        /// <returns>The help message on usege.</returns>
+        [HelpOption]
+        public string GetUsage()
+        {
+            // this without using CommandLine.Text
+            //  or using HelpText.AutoBuild
+            var usage = new StringBuilder();
+            usage.AppendLine("\nUsage:");
+            usage.AppendLine("\tDotNetNumerics [-i Iterations]\n");
+            usage.AppendLine("where");
+            usage.AppendLine("\tIterations\tThe number of calculation iterations to run. Default value is 10000000.\n");
+            return usage.ToString();
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,18 @@
 # DotNetNumericsPerfTest
-Fiddling with System.Numerics.Vectors and SIMD
+This example tries to utilize the [SIMD capabilities](http://instil.co/2016/03/21/parallelism-on-a-single-core-simd-with-c/) (SSE of AVX extensions) of contemporary CPUs using the `System.Numerics.Vectors` namespace which is a feature of .NET's [x64 RyuJIT](https://blogs.msdn.microsoft.com/dotnet/2013/09/30/ryujit-the-next-generation-jit-compiler-for-net/).
+
+## Dependencies ##
+* [.NET 4.6.2 Developer Pack](https://www.microsoft.com/en-us/download/details.aspx?id=53321)
+* Either [Visual Studio](https://www.visualstudio.com/vs/) 2015 (or any newer version) *or* [Microsoft Build Tools 2015](https://www.microsoft.com/en-us/download/details.aspx?id=48159) (or any newer release)
+
+## System Requirements ##
+* **x64** CPU with *SSE* or *AVX* extensions running an **x64** Windows OS, 
+* [.NET 4.6.2 Framework](https://www.microsoft.com/en-us/download/details.aspx?id=53345)
+
+## Summary of set up
+* `git clone git@github.com:laszlolukacs/DotNetNumericsPerfTest.git <LOCAL_WORKING_DIR>`
+* Open the `DotNetNumericsPerfTest.sln` in Visual Studio
+* Build the solution
+
+## Basic Usage
+* From the output directory: `DotNetNumericsPerfTest.exe --i <NUMBER_OF_ITERATIONS>`, where `NUMBER_OF_ITERATIONS` is the number of test iterations
